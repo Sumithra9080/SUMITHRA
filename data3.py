8fb76fd91aeb1ab834d469391904e3c0231ee508
M. sumithra <sumithramuthumuthu2006@gmail.com>
2026-07-27 21:20:26 +0530

    Add files via upload

---
+++ data3.py
@@ -0,0 +1,197 @@
+'''name = input("Enter your name: ")
+print(name.upper())'''
+
+
+
+'''sentence = input("Enter a sentence: ")
+print(sentence.lower())'''
+
+
+
+''''sentence = input("Enter a sentence: ")
+print(sentence.capitalize())'''
+
+
+
+
+'''word = input("Enter a word: ")
+char = input("Enter a character: ")
+print(word.index(char))'''
+
+
+'''sentence = input("Enter a sentence: ")
+sub = input("Enter a substring: ")
+print(sentence.find(sub))'''
+
+
+
+'''word = input("Enter a word: ")
+print(word.endswith("ing"))'''
+
+
+
+
+'''text = input("Enter a string: ")
+print(text.encode("utf-8"))'''
+
+
+'''text = input("Enter a string: ")
+print(text.isdigit())'''
+
+
+
+
+'''text = input("Enter a string: ")
+print(text.isalnum())'''
+
+
+
+
+
+''''text = input("Enter a string: ")
+print(text.isalpha())''''
+
+
+
+#list
+'''num = [10, 20, 30, 40, 50]
+
+num.append(60)
+num.insert(3, 35)
+num.remove(60)
+num.pop()
+num.extend([70,80])
+
+print(num)
+'''
+
+
+'''num = [10, 20, 10, 30, 10, 40]
+n = int(input("Enter number: "))
+print(num.count(n))'''
+
+
+
+''''num = [10, 20, 30, 40, 50]
+n = int(input("Enter number: "))
+print(num.index(n))'''
+
+
+
+
+'''num = [10, 20, 30, 40, 50]
+num.reverse()
+print(num)'''
+
+
+'''num = [50, 20, 10, 40, 30]
+
+num.sort()
+print("Ascending:", num)
+
+num.sort(reverse=True)
+print("Descending:", num)'''
+
+
+
+'''num = [10, 20, 30, 40]
+num.clear()
+print(num)'''
+
+
+
+'''num = [10, 20, 30, 40]
+new = num.copy()
+print(new)''''
+
+
+
+
+
+#tuple
+'''t = (10, 20, 30, 40, 50)
+
+print("Length:", len(t))
+print("Maximum:", max(t))
+print("Minimum:", min(t))'''
+
+'''t = (10, 20, 10, 30, 10, 40)
+n = int(input("Enter number: "))
+print(t.count(n))'''
+
+
+'''t = (10, 20, 30, 40, 50)
+n = int(input("Enter number: "))
+print(t.index(n))'''
+
+
+'''num = (10, 20, 30, 40, 50, 60, 70)
+print(num[2:6])'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
