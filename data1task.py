db869a06a8146eae9ee0808667815de03f532c67
M. sumithra <sumithramuthumuthu2006@gmail.com>
2026-07-27 20:44:25 +0530

    Add files via upload

---
+++ data1task.py
@@ -0,0 +1,156 @@
+'''a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+print(a//b)'''
+
+
+
+'''a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+c=int(input("enter the number:"))
+average=(a+b+c)/3
+print("average=",average)'''
+
+
+
+
+'''a=int(input("enter the numbe:"))
+b=int(input("enter the numbe:"))
+print(a==b)
+print(a<b)
+print(a<=b)'''
+
+
+'''a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+a=a+b
+b=a-b
+a=a-b
+print("swapping")
+print(a)
+print(b)'''
+
+
+
+
+'''x=10
+x+=5
+print(x)
+x-=3
+print(x)
+x*=2
+print(x)
+x/=4
+print(x)
+x%=2
+print(x)
+x**=3
+print(x)'''
+
+
+
+
+
+'''username=input("enter the user name:")
+password=input("enter the password:")
+if username =="admin" and password =="123":
+    print("succfully")
+else:
+    print("no valuse")'''
+
+
+
+
+'''p=float(input("enter the principal:"))
+r=float(input("enter the rate:"))
+t=float(input("enter the time:"))
+SI=(p*r*t)/100
+CI=p*((1+r/100)**t)-p
+print("simple:",SI)
+print("compont:",CI)'''
+
+
+
+
+'''import math
+num=float(input("enter a number:"))
+print("root:",math.sqrt(num))'''
+
+
+
+
+
+'''num=float(input("enter the number:"))
+cube_root=num**(1/3)
+print("cube root=",cube_root)'''
+
+
+
+
+
+
+
+
+
+
+
+
+'''length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+
+area = length * breadth
+perimeter = 2 * (length + breadth)
+
+print("Area =", area)
+print("Perimeter =", perimeter)'''
+
+
+
+'''side = float(input("Enter side: "))
+
+area = side * side
+perimeter = 4 * side
+
+print("Area =", area)
+print("Perimeter =", perimeter)'''
+
+
+
+
+
+
+
+'''
+radius = float(input("Enter radius: "))
+
+area = 3.14 * radius * radius
+circumference = 2 * 3.14 * radius
+
+print("Area =", area)
+print("Circumference =", circumference)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
