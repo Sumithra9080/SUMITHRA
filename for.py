d83c90b91925d0f4701084cc48db762c4ce4586c
M. sumithra <sumithramuthumuthu2006@gmail.com>
2026-07-27 20:39:04 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,112 @@
+'''for i in range(1,11):
+    print(i)'''
+    
+
+
+
+'''a=int(input("enter the a number:"))
+for i in range(1,11):
+    print(a,"x",i,"=",a*i)'''
+
+
+
+
+'''for i in range(2,51,2):
+    print(i)'''
+
+
+
+'''a=int(input("enter the number:"))
+fact=1
+for i in range(1,a+1):
+    fact=fact*i
+print("values=",fact)'''
+
+
+
+
+'''for i in range(1,11):
+    print("*" *i)'''
+
+
+
+'''for i in range(2,21,2):
+    print(i)'''
+
+
+
+'''for i in range(1,11):
+    print(i)'''
+
+
+'''for i in range(1,21):
+    print(i)'''
+
+
+
+'''for i in range(50,0,-1):
+    print(i)'''
+
+'''num=int(input("enter the number(1 to 100):"))
+for i in range(1,num+1):
+    if i%9==0:
+        print("loop",i)
+        break
+    if i%3==0:
+        continue
+    print(i)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
