465ba857fed732ef8dea589ae231a0b07bfa5372
M. sumithra <sumithramuthumuthu2006@gmail.com>
2026-09-22 21:40:43 +0530

    Add files via upload

---
+++ modules1.py
@@ -0,0 +1,120 @@
+#pywhatkit modules
+
+
+#import pywhatkit as kit
+kit.sendwhatmsg("+919789698778", "Hello da!", 20, 35)
+kit.sendwhatmsg("+916379154599", "Hello", 15, 30) # 3:30 PM
+kit.sendwhatmsg_instantly("+911234567890", "Instant message sent!", wait_time=10)
+kit.sendwhatmsg_to_group("AB1236EF", "Hello Group!", 16, 45) # Replace with your group ID
+kit.search("Python vs Java")
+kit.info("Taj Mahal", lines=3)
+kit.shutdown(60) # Shutdown in 60 seconds
+kit.cancel_shutdown()
+kit.send_mail("youremail@gmail.com", "yourpassword", "Subject", "Message")
+kit.playonyt("Imagine Dragons Believer")
+import pywhatkit as kit
+kit.playonyt("comady")
+kit.search("python vs java")
+##
+
+
+
+#math modules
+
+square root
+import math
+n=25
+print(math.sqrt(n))
+print(math.pow(2,3))
+print(math.factorial(5))
+n=5.7
+print(math.ceil(n))
+print(math.floor(n))
+print(math.gcd(12,18))
+
+
+
+#scipy
+
+
+from scipy import stats
+data=[10,20,30,40,50]
+print(stats.mode(data))
+print("mean:",stats.tmean(data))
+print("median:",stats.scoreatpercentile(data,50))
+print("mode:",stats.mode(data))
+##
+
+
+
+#Time
+time.time()
+time.sleep()
+time.ctime()
+time.locatime()
+time.mktime()
+time.gmtime()
+time.strptime()
+time.strftime()
+time.asctime()
+
+#datTime
+datetime.now()
+datetime.date()
+datetime.time()
+datetime.timedelta()
+datetime.strftime()
+datetime.strptime()
+datetime.today()
+
+
+
+#calender module
+calender()
+month()
+isleap (year)
+leapday(year1,year2)
+weekday(y,m,d)
+monthcalender(y,m)
+
+
+
+
+#MATH modules:-
+import math
+
+print(math.pi)            # 3.145
+print(math.sqrt(9))       # 3.0
+print(math.acos(1))       # 0.0
+print(math.atan(90))      # 1.55
+print(math.ceil(3.4))     # 4
+print(math.degrees(7))    # 401.070
+print(math.factorial(6))  # 720
+print(math.exp(7))        # 1096.0
+print(math.expm1(8))      # 2979...
+print(math.fabs(2))       # 2.0
+print(math.floor(75.8))
+print(math.gcd(8, 12))
+print(math.hypot(3, 4))
+print(math.inf)
+print(math.isqrt(7))
+print(math.lcm(90, 120))
+print(math.log(5))
+print(math.log10(5))
+print(math.modf(89.56))
+print(math.nan)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
